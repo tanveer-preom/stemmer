e75bbecfe78807554631112b92f18b47714aebfd
Tanveer Islam <tanveer.preom@gmail.com>
2017-08-23 00:11:45 +0600

    finalized

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-$url = 'http://' . $_SERVER['HTTP_HOST'];            // Get the server
-$url .= rtrim(dirname($_SERVER['PHP_SELF']), '/\\'); // Get the current directory
-$url .= '/public/';            // <-- Your relative path
-header('Location: ' . $url, true, 302); 
-?>
